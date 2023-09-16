8f3a3ce8ffbf20a32a8c66436ee151598f46234f
Tejaswi Gowda <tejaswi@asu.edu>
2023-09-16 13:27:28 -0700

    ac

---
+++ app.js
@@ -0,0 +1,35 @@
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://54.203.235.138:3000/bvh_server');
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send("connected");
+});
+
+
+var dgram = require("dgram");
+var udpServer = dgram.createSocket("udp4");
+
+udpServer.on("error", function (err) {
+    console.log("udpServer error:\n" + err.stack);
+    udpServer.close();
+    }
+);
+
+udpServer.on("message", function (msg, rinfo) {
+    console.log("udpServer got: " + msg + " from " + rinfo.address + ":" + rinfo.port + "\n");
+    ws.send(msg);
+    //var x = msg.toString().trim().split(" ").join(" ");
+    //console.log(JSON.parse(msg).scene.actors);
+    }
+);
+
+udpServer.on("listening", function () {
+    var address = udpServer.address();
+    console.log("udpServer listening " + address.address + ":" + address.port);
+    }
+);
+
+udpServer.bind(14043);
